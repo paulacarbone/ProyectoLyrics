ed12d59264af42aab925863e9a2203c10e9f7dc9
unknown <lucasboniardi@gmail.com>
2020-11-23 19:59:57 -0300

    Comienzo de desarrolo de JS con API's

---
+++ js/canciones.js
@@ -0,0 +1,10 @@
+const d = document,
+$form = d.getElementById("song-search"),
+$error = d.querySelector(".error"),
+$main = d.querySelector(".main"),
+$artist = d.querySelector(".artist"),
+$song = d.querySelector(".song");
+
+$form.addEventListener("submit", async e => {
+
+ });
